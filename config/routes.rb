2fac9ccef201606904321e362c769bb58f2d14f0
Philip <philipvuong@yahoo.com>
2019-02-05 02:22:31 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'quotes#about'
   root 'quotes#index'
   resources :quotes
-  get 'about', to: 'quotes#about'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
+
 end
