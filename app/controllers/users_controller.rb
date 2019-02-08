032e6148e3acb711dd31f94df19760b8b0f762b2
Philip <philipvuong@yahoo.com>
2019-02-08 19:25:18 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Splurty!"
       redirect_to @user # same as 'redirect_to user_url(@user)'
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about', to: 'quotes#about'
   root 'quotes#index'
@@ -8,5 +6,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   resources :users
-
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
